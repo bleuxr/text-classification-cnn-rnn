d29757128092ff49c880c48f0c4636347cb0d766
bleuxr <38353817+bleuxr@users.noreply.github.com>
2019-05-21 21:37:09 +0800

    修改类型数量

--- rnn_model.py
+++ rnn_model.py
@@ -10,7 +10,7 @@ class TRNNConfig(object):
     # 模型参数
     embedding_dim = 64      # 词向量维度
     seq_length = 600        # 序列长度
-    num_classes = 10        # 类别数
+    num_classes = 5        # 类别数
     vocab_size = 5000       # 词汇表达小
 
     num_layers = 2           # 隐藏层层数
